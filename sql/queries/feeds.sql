35ab58834d3411523a7378ea76abbbc6299759e1
Daniil Galtsev <galtsev.daniil@gmail.com>
2023-10-20 13:39:41 +0000

    add last_fetched_at feeds queries

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -8,3 +8,19 @@ SELECT * FROM feeds;
 
 -- name: GetFeed :one
 SELECT * FROM feeds WHERE id = $1;
+
+-- name: GetNextFeedsToFetch :many
+SELECT * FROM feeds
+WHERE (
+    (last_fetched_at < NOW()) OR
+    (last_fetched_at IS NULL)
+)
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT $1;
+
+-- name: UpdateLastFetchedAt :exec
+UPDATE feeds
+SET
+ last_fetched_at = NOW(),
+ updated_at = NOW()
+WHERE id=$1;
